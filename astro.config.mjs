9dffbc958efeb4abd56f6c3ddf2ad5612e12c03b
Justin Bennett <justin.bennett@norm.ai>
2025-12-15 21:08:37 -0600

    Fixup style imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
       remarkPlugins: [remarkMdc, remarkMdcToMdx],
     }),
     sitemap(),
-    UnoCSS({ injectReset: true }),
+    UnoCSS(),
   ],
   vite: {
     resolve: {
